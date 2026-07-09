84a4299a990a95c0b3493e8b8b07687d759557d2
CAETANO FAGUNDES BRAGA <caetano_f_braga@estudante.sesisenai.org.br>
2026-07-09 14:26:17 -0300

    javascript-aula2

---
+++ JavaScript/aula-02/aula-02-atividades/atividade-15.js
@@ -0,0 +1,21 @@
+let nome = "Caetano";
+let idade = 15;
+let altura = 1.83;
+let peso = 85;
+
+let imc = peso / (altura * altura);
+
+let maiorIdade = idade >= 18;
+let imc25 = imc < 25;
+let nomeMaior3 = nome.length > 3;
+
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade} anos`);
+console.log(`Altura: ${altura} m`);
+console.log(`Peso: ${peso} kg`);
+console.log(`IMC: ${imc.toFixed(2)}`);
+
+console.log(`A pessoa é maior de idade? ${maiorIdade}`);
+console.log(`O IMC está abaixo de 25? ${imc25}`);
+console.log(`O nome tem mais de 3 letras? ${nomeMaior3}`);
